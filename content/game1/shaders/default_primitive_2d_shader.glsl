c41798c9812212ce36f32c3e80ce0ece80935dad
BerryVanSomeren <berryvansomeren@gmail.com>
2020-01-15 19:37:56 +0100

    Temporarily disabled 2D model and projection matrix uniformsin the default 2D shader, because the 2D camera doesn't work due to refactoring.

--- content/game1/shaders/default_primitive_2d_shader.glsl
+++ content/game1/shaders/default_primitive_2d_shader.glsl
@@ -1,16 +1,15 @@
-
 #ifdef SHAKE_VERTEX_SHADER
 //================================================================
 // VERTEX
 
 layout (location = SHAKE_VERTEX_POS2) in vec2 in_pos2;
 
-uniform mat4 u_SHAKE_MODEL;
-uniform mat4 u_SHAKE_PROJECTION;
+//uniform mat4 u_SHAKE_MODEL;
+//uniform mat4 u_SHAKE_PROJECTION;
 
 void main()
 {
-    gl_Position = u_SHAKE_PROJECTION * u_SHAKE_MODEL * vec4( in_pos2, 0.f, 1.f );
+    gl_Position = vec4( in_pos2, 0.f, 1.f );
 }
 
 #endif
